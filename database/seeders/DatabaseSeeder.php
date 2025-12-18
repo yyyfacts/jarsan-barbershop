06a5a3683d049c521c001137a94e49587a2f7ff4
Zain Aufa <zainaufa99@gmail.com>
2025-12-19 00:44:43 +0700

    paksa https dan cookie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,30 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 1. BUAT AKUN ADMIN (Wajib pakai email ini biar jadi admin)
+        User::create([
+            'name' => 'Admin Jarsan',
+            'email' => 'admin@jarsan.com', // JANGAN DIGANTI (Ini kuncinya)
+            'password' => Hash::make('password123'), // Passwordnya ini
+            'email_verified_at' => now(),
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 2. BUAT AKUN USER BIASA (Buat ngetes tampilan user)
+        User::create([
+            'name' => 'Pelanggan Setia',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
         ]);
     }
 }
